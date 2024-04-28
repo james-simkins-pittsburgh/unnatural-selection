6995fb12cda1a19a179af03b7ccf56dbefc43b77
James Simkins <jamesmsimkins@gmail.com>
2024-04-28 11:52:38 -0400

    Added bevy_framepace + testing mode
    - Added bevy-framepace
    - Added an easy way to run test code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // This hides the command prompt
-// #![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
 use bevy::prelude::*;
 use bevy_embedded_assets::EmbeddedAssetPlugin;
@@ -10,18 +10,38 @@ pub mod network_communicator;
 pub mod simulation;
 pub mod computer_players;
 pub mod utility_functions;
+// Scaffold code is code that exists to stand in for parts of coe that haven't been written yet.
 pub mod scaffold_code;
+// Test code is code that exists to test parts of code without initializing the app.
 pub mod test_code;
 
-/* Set as constant for testing purposes
-This will eventually not be a constant */
+// Set as constant for testing purposes This will eventually not be a constant
+const MAP_WIDTH: usize = 10000;
+const MAP_MAX_HEIGHT: usize = 19000;
 
-const MAP_WIDTH:usize = 250000;
-const MAP_MAX_DEPTH:usize = 150000;
+/* This is a toggle that allows quick testing of test code.
+Delete the following line before release. */
+const USE_TEST_CODE: bool = false;
 
 fn main() {
-    App::new()
-        .add_plugins((EmbeddedAssetPlugin::default(), DefaultPlugins))
-        .insert_resource(Time::<Fixed>::from_hz(60.0))
-        .run();
+    // Delete starting here to remove test code runner before release.
+
+    if USE_TEST_CODE {
+        crate::test_code::run_test_code()
+    } else {
+        // End delete here.
+
+        App::new()
+            .add_plugins((EmbeddedAssetPlugin::default(), DefaultPlugins))
+            .add_plugins(bevy_framepace::FramepacePlugin)
+            .add_systems(Startup, framepace_setup)
+            .run();
+        // Delete following "}" before release.
+    }
+}
+
+/* This limits the framerate to 30 fps. This is a deliberate decision to deemphasize graphic quality
+in order to double the potential size of the simulation since this is a CPU bound appllication. */
+fn framepace_setup(mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
+    settings.limiter = bevy_framepace::Limiter::from_framerate(30.0);
 }
--- src/test_code.rs
+++ src/test_code.rs
@@ -1,1 +1,5 @@
 pub mod test_cheap_random;
+
+pub fn run_test_code() {
+    // Insert a function call to the test code here.
+}
