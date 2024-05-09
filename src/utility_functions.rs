dfa3c1603ca5a6d4efe74fca0fa490a14aa079a6
James Simkins <131498226+james-simkins-pittsburgh@users.noreply.github.com>
2024-05-09 19:46:56 -0400

    Added Sine Trig Table
    Progress on deterministic trig

--- src/utility_functions.rs
+++ src/utility_functions.rs
@@ -1,2 +1,5 @@
 // This module provides a cheaper alternative to using the random number generator every time.
 pub mod cheap_random;
+// This module provides deterministic trignometry
+pub mod deterministic_trig;
+
