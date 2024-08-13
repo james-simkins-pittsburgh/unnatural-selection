c584459e87629369a58cfb08f0361cc6c7643add
James Simkins <jamesmsimkins@gmail.com>
2024-08-13 07:15:13 -0400

    Add: Collision detection logic (incomplete)

--- src/simulation/biosphere_simulation/collision_calculations/collision_detector.rs
+++ src/simulation/biosphere_simulation/collision_calculations/collision_detector.rs
@@ -4,12 +4,12 @@ use crate::{
         biosphere_simulation::blob_mover::CollisionCheckResult,
         AllBiosphereInformation,
         CircleEntityType,
+        CirclePositionRecord,
     },
 };
 
-struct CircleCheckResults {
+struct CollisionCheckResults {
     collision: bool,
-    collision_pairs_vec: Vec<(CircleInfo, CircleInfo)>,
     x_move: i32,
     y_move: i32,
     rotation_in_thousandth_radians: i32,
@@ -20,8 +20,6 @@ struct CircleInfo {
     x: i32,
     y: i32,
     radius: i32,
-    entity_type: CircleEntityType,
-    id_number: usize,
 }
 
 pub fn detect_collision(
@@ -31,13 +29,13 @@ pub fn detect_collision(
 ) -> CollisionCheckResult {
     let mut collider_circles: Vec<CircleInfo> = Vec::new();
 
+    // This makes a vec of all the circles of the collider.
+
     for organism_number in all_biosphere_information.blob_vec[blob_number].blob_members.iter() {
         collider_circles.push(CircleInfo {
             x: all_biosphere_information.organism_information_vec[*organism_number].x_location,
             y: all_biosphere_information.organism_information_vec[*organism_number].y_location,
             radius: all_biosphere_information.organism_information_vec[*organism_number].y_location,
-            entity_type: CircleEntityType::Organism,
-            id_number: *organism_number,
         });
 
         if all_biosphere_information.organism_information_vec[*organism_number].oblong {
@@ -48,33 +46,111 @@ pub fn detect_collision(
                     x: circle.x,
                     y: circle.y,
                     radius: circle.radius,
-                    entity_type: CircleEntityType::Organism,
-                    id_number: *organism_number,
                 });
             }
         }
     }
 
-    // Send all the circles to the helper function to work out what, if anything, collides first.
-    let collision_check_result = check_circles(
-        collider_circles,
-        &all_biosphere_information,
-        &game_settings
-    );
-
-    // Send the results back to the blob mover.
-    return CollisionCheckResult {
-        collision: collision_check_result.collision,
-        x_move: collision_check_result.x_move,
-        y_move: collision_check_result.y_move,
-        rotation_in_thousandth_radians: collision_check_result.rotation_in_thousandth_radians,
-        involved_blobs: collision_check_result.involved_blobs,
-        involved_minerals: collision_check_result.involved_minerals,
-    };
+    // Send all the circles to the helper function to work out what, if anything, collides first
+    // Then send the results back to the blob mover.
+    return check_circles(collider_circles, &all_biosphere_information, &game_settings, blob_number);
 }
+
 // This helper function consults the detection grid to determine if any collisions will occur with the movement.
 fn check_circles(
     collider_circles: Vec<CircleInfo>,
     all_biosphere_information: &AllBiosphereInformation,
-    game_settings: &GameSettings
-) -> CollisionCheckResult {}
+    game_settings: &GameSettings,
+    blob_number: usize
+) -> CollisionCheckResult {
+    let mut x_move = all_biosphere_information.blob_vec[blob_number].blob_x_velocity;
+    let mut y_move = all_biosphere_information.blob_vec[blob_number].blob_y_velocity;
+    let mut r_move = all_biosphere_information.blob_vec[blob_number].angular_velocity;
+
+    let original_x_move = x_move;
+    let original_y_move = y_move;
+
+    let mut involved_blobs = vec![blob_number];
+    let mut involved_minerals = vec![];
+
+    // Iterate over every collider circle.
+    for collider_circle in collider_circles.iter() {
+        // Iterate over every collidee circle in the detection grid.
+        for collidee_circle in all_biosphere_information.collision_detection_grid[
+            ((collider_circle.x - game_settings.map_length / 2) / 10000) as usize
+        ][((collider_circle.x - game_settings.map_length / 2) / 10000) as usize].iter() {
+            check_two_circles_translational(
+                &mut x_move,
+                &mut y_move,
+                &mut involved_blobs,
+                &mut involved_minerals,
+                blob_number,
+                collider_circle,
+                collidee_circle
+            );
+        }
+    }
+
+    return CollisionCheckResult {
+        collision: todo!(),
+        x_move: todo!(),
+        y_move: todo!(),
+        rotation_in_thousandth_radians: todo!(),
+        involved_blobs: todo!(),
+        involved_minerals: todo!(),
+    };
+}
+
+fn check_two_circles_translational(
+    mut x_move: &mut i32,
+    mut y_move: &mut i32,
+    mut involved_blobs: &mut Vec<usize>,
+    mut involved_minerals: &mut Vec<usize>,
+    blob_number: usize,
+    collider_circle: &CircleInfo,
+    collidee_circle: &CirclePositionRecord
+) {
+    if
+        collidee_circle.identity_number != blob_number ||
+        collidee_circle.circle_entity_type != CircleEntityType::Organism
+    {
+        if
+            (collidee_circle.center_x - (collider_circle.x + *x_move)) *
+                (collidee_circle.center_x - (collider_circle.x + *x_move)) +
+                (collidee_circle.center_y - (collider_circle.y + *y_move)) *
+                    (collidee_circle.center_y - (collider_circle.y + *y_move)) <
+            (collidee_circle.radius + collider_circle.radius) *
+                (collidee_circle.radius + collider_circle.radius)
+        {
+            if
+                (collidee_circle.center_x -
+                    collider_circle.x -
+                    (collidee_circle.radius + collider_circle.radius) * 1000) /
+                    *x_move >
+                (collidee_circle.center_y -
+                    collider_circle.y -
+                    (collidee_circle.radius + collider_circle.radius) * 1000) /
+                    *y_move
+            {
+
+                // Left off here
+
+                if
+                    collidee_circle.center_x -
+                        collider_circle.x -
+                        (collidee_circle.radius + collider_circle.radius) == *x_move
+                {
+                    // Logic Here
+                } else if
+                    collidee_circle.center_x -
+                        collider_circle.x -
+                        (collidee_circle.radius + collider_circle.radius) < *x_move
+                {
+                    // Logic Here
+
+                }
+            } else {
+            }
+        }
+    }
+}
