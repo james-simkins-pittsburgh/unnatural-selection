0899268f6d94d182a7193cefa09593a3dd2a7e64
James Simkins <jamesmsimkins@gmail.com>
2024-08-17 09:33:48 -0400

    Add: Comments to collision detector

--- src/simulation/biosphere_simulation/collision_calculations/collision_detector.rs
+++ src/simulation/biosphere_simulation/collision_calculations/collision_detector.rs
@@ -9,6 +9,7 @@ use crate::{
     utility_functions::deterministic_trigonometry::DeterministicTrig,
 };
 
+// This stores the circle information for the colliders and collidees.
 struct CircleInfo {
     x: i32,
     y: i32,
@@ -23,8 +24,7 @@ pub fn detect_collision(
 ) -> CollisionCheckResult {
     let mut collider_circles: Vec<CircleInfo> = Vec::new();
 
-    // This makes a vec of all the circles of the collider.
-
+    // This makes a vec of all the circles of the collider blob.
     for organism_number in all_biosphere_information.blob_vec[blob_number].blob_members.iter() {
         collider_circles.push(CircleInfo {
             x: all_biosphere_information.organism_information_vec[*organism_number].x_location,
@@ -56,7 +56,7 @@ pub fn detect_collision(
     );
 }
 
-// This helper function consults the detection grid to determine if any collisions will occur with the movement.
+// This helper function consults the detection grid to determine if any collisions will occur with the movement of the blob.
 fn check_circles(
     collider_circles: Vec<CircleInfo>,
     all_biosphere_information: &AllBiosphereInformation,
@@ -64,22 +64,28 @@ fn check_circles(
     blob_number: usize,
     deterministic_trig: &DeterministicTrig
 ) -> CollisionCheckResult {
+    // These store the maximum movement before a collision (if any) occurs.
     let mut x_move = all_biosphere_information.blob_vec[blob_number].blob_x_velocity;
     let mut y_move = all_biosphere_information.blob_vec[blob_number].blob_y_velocity;
     let mut r_move = all_biosphere_information.blob_vec[blob_number].angular_velocity;
 
+    // This stores the original moves so it can be references later.
     let original_x_move = x_move;
     let original_y_move = y_move;
 
+    // This is a list of all involved blobs so calculations can be done to get new velocities after a collision.
     let mut involved_blobs = vec![blob_number];
+
+    // This keeps track if a mineral is involved. If one is, then the entire collision will result in 0 velocities.
     let mut involved_minerals = false;
 
-    // Iterate over every collider circle.
+    // Iterates over every collider circle.
     for collider_circle in collider_circles.iter() {
-        // Iterate over every collidee circle in the detection grid.
+        // Iterates over every collidee circle in the detection grid.
         for collidee_circle in all_biosphere_information.collision_detection_grid[
             ((collider_circle.x - game_settings.map_length / 2) / 10000) as usize
         ][((collider_circle.x - game_settings.map_length / 2) / 10000) as usize].iter() {
+            // This function checks if the two circle collide and determined how much x and y movement occurs before that.
             check_two_circles_translational(
                 &mut x_move,
                 &mut y_move,
@@ -97,12 +103,13 @@ fn check_circles(
 
     let collision: bool;
 
+    // If there are involved blobs other than the blob, then a collision must have happened.
     if involved_blobs.len() > 1 || involved_minerals == true {
         collision = true;
     } else {
         collision = false;
     }
-
+    
     return CollisionCheckResult {
         collision,
         x_move,
@@ -132,13 +139,14 @@ fn check_two_circles_translational(
     {
         // If they're going to collide with less than the current x and y moves.
         if
-            ((collidee_circle.center_x - (collider_circle.x + *x_move)) *
+            (collidee_circle.center_x - (collider_circle.x + *x_move)) *
                 (collidee_circle.center_x - (collider_circle.x + *x_move)) +
                 (collidee_circle.center_y - (collider_circle.y + *y_move)) *
                     (collidee_circle.center_y - (collider_circle.y + *y_move)) <
             (collidee_circle.radius + collider_circle.radius) *
-                (collidee_circle.radius + collider_circle.radius))
+                (collidee_circle.radius + collider_circle.radius)
         {
+            // Then previously added collisions will no longer happen.
             if collidee_circle.circle_entity_type == CircleEntityType::Organism {
                 *involved_blobs = vec![blob_number, collidee_circle.identity_number];
                 *involved_minerals = false;
@@ -146,28 +154,6 @@ fn check_two_circles_translational(
                 *involved_blobs = vec![blob_number];
                 *involved_minerals = true;
             }
-
-            if y_move > x_move {
-
-                while ((collidee_circle.center_x - (collider_circle.x + *x_move)) *
-                (collidee_circle.center_x - (collider_circle.x + *x_move)) +
-                (collidee_circle.center_y - (collider_circle.y + *y_move)) *
-                    (collidee_circle.center_y - (collider_circle.y + *y_move)) <
-            (collidee_circle.radius + collider_circle.radius) *
-                (collidee_circle.radius + collider_circle.radius)) && *x_move != 0 {
-
-                    // Left off here
-
-
-
-
-                }
-
-
-
-            }
-
-          
         }
     }
 }
