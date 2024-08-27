e34a9e9d919c4fecffaec11aafc7c92e65cf4b29
James Simkins <jamesmsimkins@gmail.com>
2024-08-27 18:03:53 -0400

    Fix: Heron's Method
    Fixed an error with Heron's method that was generating an infinite loop. The collision detector is working properly now!

--- src/utility_functions/integer_math.rs
+++ src/utility_functions/integer_math.rs
@@ -10,41 +10,91 @@ pub fn square_root(input: i64) -> i64 {
     // Application of Heron's method.
 
     let mut power_of_100: u32 = 1;
-    let one_hundred: i64 = 100;
-    let ten: i64 = 10;
-
     // Figure out the magnitude of the answer and make an estimate.
-    while one_hundred.pow(power_of_100) < input {
+    while i64::pow(100i64, power_of_100) < input {
         power_of_100 = power_of_100 + 1;
-        println!("Help, I am stuck!");
     }
 
     let mut estimate;
+    let mut previous_estimate = -1;
 
-    if input / one_hundred.pow(power_of_100 - 1) < 10 {
-        estimate = 2 * ten.pow(power_of_100 - 1);
+    if input / i64::pow(100i64, power_of_100 - 1) < 10 {
+        estimate = 2 * i64::pow(10i64, power_of_100 - 1);
     } else {
-        estimate = 6 * ten.pow(power_of_100 - 1);
+        estimate = 6 * i64::pow(10i64, power_of_100 - 1);
     }
 
     // Repeat until the estimate is a better estimate that the integer above or below it.
     while
-        (input - estimate * estimate).abs() <= (input - (estimate + 1) * (estimate + 1)).abs() &&
-        (input - estimate * estimate).abs() <= (input - (estimate - 1) * (estimate - 1)).abs()
+        (input - estimate * estimate).abs() > (input - (estimate + 1) * (estimate + 1)).abs() ||
+        (input - estimate * estimate).abs() > (input - (estimate - 1) * (estimate - 1)).abs()
     {
         estimate = (estimate + input / estimate) / 2;
-        println!("Help, I am stucker!");
-        println!("{}", estimate);
+        if estimate == previous_estimate {
+            // This extra code is needed because it gets stuck if the answer is closer to one larger because of integer division.
+            if
+                (input - estimate * estimate).abs() >
+                (input - (estimate + 1) * (estimate + 1)).abs()
+            {
+                return estimate + 1;
+            } else {
+                return estimate;
+            }
+        }
+        previous_estimate = estimate;
+    }
+    return estimate;
+}
+
+pub fn square_root_128 (input: i128) -> i128 {
+    if input <= 0 {
+        /* Negative input may occur due to rounding errors when the answer should be 0.
+        Therefore, it makes sense to return 0 in that case */
+        return 0;
     }
 
-return estimate;
+    // Application of Heron's method.
 
+    let mut power_of_100: u32 = 1;
+    // Figure out the magnitude of the answer and make an estimate.
+    while i128::pow(100i128, power_of_100) < input {
+        power_of_100 = power_of_100 + 1;
+    }
+
+    let mut estimate;
+    let mut previous_estimate = -1;
+
+    if input / i128::pow(100i128, power_of_100 - 1) < 10 {
+        estimate = 2 * i128::pow(10i128, power_of_100 - 1);
+    } else {
+        estimate = 6 * i128::pow(10i128, power_of_100 - 1);
+    }
+
+    // Repeat until the estimate is a better estimate that the integer above or below it.
+    while
+        (input - estimate * estimate).abs() > (input - (estimate + 1) * (estimate + 1)).abs() ||
+        (input - estimate * estimate).abs() > (input - (estimate - 1) * (estimate - 1)).abs()
+    {
+        estimate = (estimate + input / estimate) / 2;
+        if estimate == previous_estimate {
+            // This extra code is needed because it gets stuck if the answer is closer to one larger because of integer division.
+            if
+                (input - estimate * estimate).abs() >
+                (input - (estimate + 1) * (estimate + 1)).abs()
+            {
+                return estimate + 1;
+            } else {
+                return estimate;
+            }
+        }
+        previous_estimate = estimate;
+    }
+    return estimate;
 }
 
 #[test]
 fn test_square_root() {
-    println!("Hi There!");
     assert_eq!(square_root(38), 6);
     assert_eq!(square_root(100), 10);
-    assert_eq!(square_root(223), 15)
+    assert_eq!(square_root(223), 15);
 }
--- src/utility_functions/quadratic_solver.rs
+++ src/utility_functions/quadratic_solver.rs
@@ -1,15 +1,13 @@
 // This module solves quadratics using the quadratic formula.
 
-// use super::integer_math::square_root_128;
+use super::integer_math::square_root_128;
 
 
 
 pub fn solve_quadratic (a_x_1000: i128, b_x_1000:i128, c_x_1000:i128) -> (i64, i64) {
 
-    // return (((-b_x_1000 + square_root_128(b_x_1000 * b_x_1000 - 4 * a_x_1000 * c_x_1000 )) / (2 * a_x_1000 )) as i64  , ((-b_x_1000 - square_root_128(b_x_1000 * b_x_1000 - 4 * a_x_1000 * c_x_1000 )) / (2 * a_x_1000 )) as i64)
+    return (((-b_x_1000 + square_root_128(b_x_1000 * b_x_1000 - 4 * a_x_1000 * c_x_1000 )) / (2 * a_x_1000 )) as i64  , ((-b_x_1000 - square_root_128(b_x_1000 * b_x_1000 - 4 * a_x_1000 * c_x_1000 )) / (2 * a_x_1000 )) as i64)
 
-    return (1,1);
-    
 }
 
 #[test]
