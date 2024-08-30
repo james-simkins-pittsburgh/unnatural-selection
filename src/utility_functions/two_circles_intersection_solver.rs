219e46a26d0a4d3a0c1dad1216339cc74e9051de
James Simkins <jamesmsimkins@gmail.com>
2024-08-30 19:32:12 -0400

    Add: Circle intersection solver
    Finished the code for the circle intersection solver.

--- src/utility_functions/two_circles_intersection_solver.rs
+++ src/utility_functions/two_circles_intersection_solver.rs
@@ -1,6 +1,6 @@
-// This find the intersection points of two circles.
-
-// The code assumes the circles do intersect and does not consider non-intersecting circles.
+/* This find the intersection points of two circles. The code assumes the circles do intersect 
+and does not consider non-intersecting circles. Special thanks to Tim Voght who wrote the code in C 
+that this code was adapted from and generously placed it in the public domain. */
 
 use super::integer_math::square_root_64;
 
@@ -12,6 +12,7 @@ pub fn solve_two_circle_intersection(
     y1: i32,
     r1: i32
 ) -> ((i32, i32), (i32, i32)) {
+
     let dx = x1 - x0;
 
     let dy = y1 - y0;
@@ -21,14 +22,28 @@ pub fn solve_two_circle_intersection(
     let a: i32;
 
     if d != 0 {
-        a = (r0 * r0 - (r1 + r1) + d * d) / 2d;
+        a = (r0 * r0 - (r1 * r1) + d * d) / (2 * d);
     } else {
-        return (0, 0);
+        return ((0, 0), (0, 0));
     }
+
+    let x2 = x0 + (dx * a/d);
+
+    let y2 = y0 + (dy * a/d);
 
     let h = square_root_64((r0 * r0 - a * a) as i64) as i32;
 
-    let rx = -dy * (h / d);
+    let rx = -dy * h / d;
 
-    let ry = dx * (h / d);
+    let ry = dx * h / d;
+
+    println!("{}" , h);
+
+    return ((x2 + rx, y2 + ry),(x2 - rx, y2 - ry))
+}
+
+#[test] 
+
+fn test_circle_intersection_solver () {
+    assert_eq!(solve_two_circle_intersection (3230, 4213, 2000, 7324, 3413, 3000), ((4944,5242),(4432,2614)))
 }
