1b57832d5329b72d6e43c2be9c52ca0504c875aa
James Simkins <jamesmsimkins@gmail.com>
2024-09-13 06:18:44 -0400

    Add: Momentum Calculation Logic

--- src/simulation/biosphere_simulation/collision_calculations/organism_combination.rs
+++ src/simulation/biosphere_simulation/collision_calculations/organism_combination.rs
@@ -1,4 +1,7 @@
-use crate::{simulation::AllSpatialBiosphereInformation, utility_functions::deterministic_trigonometry::DeterministicTrig};
+use crate::{
+    simulation::AllSpatialBiosphereInformation,
+    utility_functions::deterministic_trigonometry::DeterministicTrig,
+};
 
 struct MassAndCOM {
     center_of_mass_x: i32,
@@ -10,7 +13,7 @@ pub fn apply_collision(
     all_spatial_biosphere_information: &mut AllSpatialBiosphereInformation,
     combination_list: &Vec<usize>,
     mineral_involved: bool,
-    deterministic_trig: &DeterministicTrig,
+    deterministic_trig: &DeterministicTrig
 ) {
     let mut x_momentum = 0;
     let mut y_momentum = 0;
@@ -99,21 +102,51 @@ fn calculate_momentum(
     mut x_momentum: &mut i32,
     mut y_momentum: &mut i32,
     mut r_momentum: &mut i32,
-    deterministic_trig: &DeterministicTrig,
+    deterministic_trig: &DeterministicTrig
 ) {
-
     for member_blob_number in combination_list.iter() {
+        // all_spatial_biosphere_information.blob_vec[*member_blob_number]
 
-        // all_spatial_biosphere_information.blob_vec[*member_blob_number];
+        // This calculates the angle of the line between the two centers of mass compared to the positive x axis.
+        let angle_to_center_of_mass = deterministic_trig.d_trig.arctangent((
+            (1000 *
+                (new_mass_and_center_of_mass.center_of_mass_x -
+                    all_spatial_biosphere_information.blob_vec
+                        [*member_blob_number].center_of_mass_x)) /
+                (new_mass_and_center_of_mass.center_of_mass_y -
+                    all_spatial_biosphere_information.blob_vec
+                        [*member_blob_number].center_of_mass_y),
 
-            // This decomposes the x_momentum of the blob and then adds it to the combined blob.
+            1000,
+        ));
 
-            
+        // Uses the rotation matrix. Not sure this is right.
+        let translational_component =
+            (all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_x_velocity *
+                deterministic_trig.d_trig.cosine(angle_to_center_of_mass).0 -
+                all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_y_velocity *
+                    deterministic_trig.d_trig.sine(angle_to_center_of_mass).0) /
+            1000;
+        let rotational_component =
+            (all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_x_velocity *
+                deterministic_trig.d_trig.sine(angle_to_center_of_mass).0 +
+                all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_y_velocity *
+                    deterministic_trig.d_trig.cosine(angle_to_center_of_mass).0) /
+            1000;
 
+        // Break the translational component down into x and y.
+        let translational_x_component =
+            (translational_component *
+                deterministic_trig.d_trig.cosine(angle_to_center_of_mass).0) /
+            1000;
+        let translational_y_component =
+            (translational_component * deterministic_trig.d_trig.sine(angle_to_center_of_mass).0) /
+            1000;
 
-
+        // Add the momentum contribution of the blob to the new combined bob.
+        *x_momentum += translational_x_component * all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_mass;
+        *y_momentum += translational_y_component * all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_mass;
+        *r_momentum += rotational_component * all_spatial_biosphere_information.blob_vec[*member_blob_number].blob_mass;
 
     }
-
-
 }
